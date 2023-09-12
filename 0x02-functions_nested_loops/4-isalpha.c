b53ed1db46f156634fbefb46bdbdb04d359e7e53
breezy999 <hamooo999@hotmail.com>
2023-09-12 07:29:21 -0700

    0x00. C - Hello, World

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: variable to determine if input is lowercase or uppercase
+ *
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+int _isalpha(int c)
+{
+	int i;
+	
+	i = 0;
+	
+	if (c >= 'a' && c <= 'z')
+		i = 1;
+	else if (c >= 'A' && c <= 'Z')
+		i = 1;
+	return (i);
+}
