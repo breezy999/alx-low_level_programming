106d957cd84660b3389121be14bbca9f5413eabc
breezy999 <hamooo999@hotmail.com>
2023-09-12 06:16:58 -0700

    0x00. C - Hello, World

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
