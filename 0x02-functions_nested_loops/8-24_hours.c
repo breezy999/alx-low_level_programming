367a769aecf746874393da5e16268054646e216b
breezy999 <hamooo999@hotmail.com>
2023-09-12 14:51:01 -0700

    0x00. C - Hello, World

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * jack_bauer - a function that prints every minute of 
- * the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - prints out every minute of the day
  *
+ * Return : void
  */
+
 void jack_bauer(void)
 {
 	int h, m;
