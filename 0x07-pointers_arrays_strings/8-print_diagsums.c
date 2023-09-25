dfeb5df8c5005b94c5c673a36f0a7014e6b062f1
breezy999 <hamooo999@hotmail.com>
2023-09-25 02:03:47 -0700

    0x00. C - Hello, World

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of
+ * the two diagonals of a square matrix of integers.
+ * @a: square matrix
+ * @size: size of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int lfrg = 0, rglf = size - 1;
+	int valuelfrg = 0, valuerglf = 0;
+	int *p = a;
+
+	for (; lfrg <= size * size; lfrg += (size + 1), rglf += (size - 1))
+	{
+		valuelfrg += *(p + lfrg);
+		valuerglf += *(p + rglf);
+	}
+	printf("%d, %d\n", valuelfrg, valuerglf);
+}
